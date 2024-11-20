f751b19cfdfe1d62bcd1c7723e6c9679c57be9f2
janessaperry <hello@janessaperry.com>
2024-11-19 20:58:48 -0800

    create library page component

---
+++ src/pages/Library/Library.jsx
@@ -0,0 +1,13 @@
+import "./Library.scss";
+
+function Library() {
+	return (
+		<main className="main">
+			<div className="title-wrapper">
+				<h1 className="title">Library</h1>
+			</div>
+		</main>
+	);
+}
+
+export default Library;
