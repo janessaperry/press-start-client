841e47a2a70df34e7a4e12d437c364db073e1b35
janessaperry <hello@janessaperry.com>
2024-11-23 17:11:27 -0800

    update game card component to include game information and styling for games in collection

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ChipList from "../ChipList/ChipList";
+import { GameController, Trash } from "@phosphor-icons/react";
 import "./GameCard.scss";
+import ButtonDropdown from "../ButtonDropdown/ButtonDropdown";
+import Button from "../Button/Button";
 
-function GameCard({ game }) {
+function GameCard({
+	game,
+	gameFormatOptions,
+	gameStatusOptions,
+	collectionData,
+}) {
 	return (
-		<Link className="game-card__link" to={`/game-details/${game.id}`}>
-			<div className="game-card">
+		<div className="game-card">
+			<Link className="game-card__link" to={`/game-details/${game.id}`}>
 				<div className="game-card__cover-wrapper">
 					<img
 						className="game-card__cover"
@@ -15,8 +24,30 @@ function GameCard({ game }) {
 				</div>
 
 				<div className="game-card__content-wrapper">
-					<div className="game-card__title-wrapper">
-						<p className="game-card__title">{game.name}</p>
+					<div className="game-card__header-wrapper">
+						<div className="game-card__title-wrapper">
+							<p className="game-card__title">{game.name}</p>
+							{collectionData && (
+								<div className="game-card__meta-wrapper">
+									<p className="game-card__meta">
+										{game.developer} • {game.releaseDate}
+									</p>
+
+									<div
+										className={`game-card__time-to-beat ${
+											game.timeToBeat === "n/a" && "game-card__time-to-beat--na"
+										}`}
+									>
+										<GameController
+											weight="fill"
+											className="game-card__icon game-card__icon--controller"
+										/>
+										{game.timeToBeat}
+										{game.timeToBeat !== "n/a" && " hours"}
+									</div>
+								</div>
+							)}
+						</div>
 						<div
 							className={`game-card__rating-chip ${
 								game.rating === "n/a" && "game-card__rating-chip--na"
@@ -26,13 +57,62 @@ function GameCard({ game }) {
 						</div>
 					</div>
 
+					{!collectionData && (
+						<div className="game-card__platforms">
+							<h4 className="game-card__platforms-title">Platforms</h4>
+							<ChipList chipData={game.platforms} />
+						</div>
+					)}
+
+					{collectionData && (
+						<div className="game-card__genres">
+							<h4 className="game-card__genres-title">Genres</h4>
+							<ChipList chipData={game.genres} />
+						</div>
+					)}
+				</div>
+			</Link>
+
+			{collectionData && (
+				<div className="game-card__collection-actions-wrapper">
 					<div className="game-card__platforms">
-						<h4 className="game-card__platforms-title">Platforms</h4>
-						<ChipList chipData={game.platforms} />
+						<h4 className="game-card__platforms-title">
+							Owned / Wishlisted On
+						</h4>
+
+						<div className="game-card__ownership-details">
+							<ButtonDropdown
+								name="game-console"
+								label={collectionData.gameConsole}
+								contextClasses={"btn--outline btn--dropdown"}
+								dropdownOptions={game.platforms}
+							/>
+
+							<ButtonDropdown
+								name="game-format"
+								label={collectionData.gameFormat}
+								contextClasses={"btn--outline btn--dropdown"}
+								dropdownOptions={gameFormatOptions}
+							/>
+						</div>
+					</div>
+
+					<div className="game-card__collection-actions">
+						<ButtonDropdown
+							name="game-status"
+							label={collectionData.gameStatus}
+							contextClasses={"btn--primary btn--dropdown"}
+							dropdownOptions={gameStatusOptions}
+						/>
+						<Button
+							handleBtnClick={() => console.log("DELETE")}
+							iconLeft={<Trash className="btn__icon" weight="bold" />}
+							contextClasses="btn--outline btn--warn"
+						/>
 					</div>
 				</div>
-			</div>
-		</Link>
+			)}
+		</div>
 	);
 }
 
