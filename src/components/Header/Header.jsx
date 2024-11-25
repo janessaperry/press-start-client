3bbf8c351c01ba3383f7064078e0f833860f169c
janessaperry <hello@janessaperry.com>
2024-11-25 15:21:45 -0800

    add search feature to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useDeferredValue, useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
+import axios from "axios";
 import PressStartLogo from "../../assets/logos/press-start-logo-light.svg";
 import AvatarPlaceholder from "../../assets/images/user-avatar-placeholder.png";
 import "./Header.scss";
 
 function Header() {
-	const [queryString, setQueryString] = useState("");
+	const baseUrl = import.meta.env.VITE_API_URL;
+	const [queryValue, setQueryValue] = useState("");
+	const deferredQueryString = useDeferredValue(queryValue);
+	const [searchResults, setSearchResults] = useState([]);
+
+	const getSearchResults = async (queryString) => {
+		try {
+			const response = await axios.post(`${baseUrl}/search`, {
+				query: queryString,
+			});
+			setSearchResults(response.data);
+		} catch (error) {
+			console.error("Error getting search results", error);
+		}
+	};
+
+	useEffect(() => {
+		if (deferredQueryString?.length >= 3) {
+			const debounceTimeout = setTimeout(() => {
+				getSearchResults(deferredQueryString);
+			}, 250);
+			return () => clearTimeout(debounceTimeout);
+		} else {
+			setSearchResults([]);
+		}
+	}, [deferredQueryString]);
 
 	return (
 		<header className="header">
@@ -22,9 +48,33 @@ function Header() {
 					className="header__search-input"
 					type="text"
 					placeholder="Search games..."
-					onChange={(e) => setQueryString(e.target.value)}
-					value={queryString}
+					onChange={(e) => setQueryValue(e.target.value)}
+					value={queryValue}
 				/>
+				{searchResults.length > 0 && (
+					<ul className="header__search-results-list">
+						{searchResults?.map((result) => {
+							return (
+								<li key={result.id} className="header__search-result-item">
+									<Link
+										to={`/game-details/${result.id}`}
+										className="header__search-result-link"
+										onClick={() => setQueryValue("")}
+									>
+										{result?.cover && (
+											<img
+												src={result.cover}
+												alt="Cover"
+												className="header__search-result-cover"
+											/>
+										)}
+										{result.name}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				)}
 			</div>
 
 			<nav className="header__nav">
