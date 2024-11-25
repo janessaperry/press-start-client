fb76e2a347af91510a331603c2b14ec61b3f37e6
janessaperry <hello@janessaperry.com>
2024-11-25 11:04:00 -0800

    update nav link routes in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
 						</NavLink>
 					</li>
 					<li className="header__nav-item">
-						<NavLink className="header__nav-link" to="/collection/1">
+						<NavLink className="header__nav-link" to="/collection">
 							My Collection
 						</NavLink>
 					</li>
