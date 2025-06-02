15e62827a7470d97d01715cd1eb4471566560c17
janessaperry <hello@janessaperry.com>
2025-06-01 21:17:53 -0700

    create placeholder page for log in

---
+++ src/pages/LogInPage/LogInPage.tsx
@@ -0,0 +1,47 @@
+import PressStartLogo from "../../assets/logos/press-start-logo--dark.svg"
+import { Link } from "react-router-dom";
+import { Button, Description, Field, Fieldset, Input, Label } from "@headlessui/react";
+
+function LogInPage () {
+  return (
+    <>
+      <main className="h-screen">
+        <div className="h-full flex">
+          <div className={`flex-1 p-12`}>
+            <img src={PressStartLogo} alt="Press Start logo"/>
+          </div>
+
+          <section className="bg-secondary flex-1 p-12">
+            <p>No account? <Link to="/sign-up" className="text-link-tertiary">Create account</Link></p>
+
+            <form>
+              <h1>Sign up</h1>
+
+              <Fieldset className="flex flex-col gap-8 border-none">
+                <Field className={`flex flex-col gap-1`}>
+                  <Label htmlFor="email"
+                  >Email <span>*</span></Label>
+                  <Input id="email" type="email" placeholder="Email"/>
+                </Field>
+
+                <Field className={`flex flex-col gap-1`}>
+                  <Label htmlFor="password"
+                  >Password <span>*</span></Label>
+                  <Input id="password" type="password" placeholder="Password"/>
+                  <Description className="mb-0 text-sm text-secondary">Password should be at least 8 characters
+                    including a number and a lowercase letter.</Description>
+                </Field>
+
+              </Fieldset>
+
+              <Button>Sign In</Button>
+            </form>
+          </section>
+
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default LogInPage;
