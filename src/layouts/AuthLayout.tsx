c7b2ebb946cffed2e80af9d0150d09ea3985b56a
janessaperry <hello@janessaperry.com>
2025-06-04 11:17:15 -0700

    update AuthLayout and ProtectedRoute to useAuth for authentication check

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth.tsx";
 
 const AuthLayout = () => {
-  const isAuthenticated = true;
+
+  const { isAuthenticated } = useAuth();
+  console.log(isAuthenticated);
 
   if ( isAuthenticated ) {
     return <Navigate to="/"/>;
