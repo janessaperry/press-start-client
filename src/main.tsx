6b684602625c1066c18a5cbcc7be10aa5d47510a
janessaperry <hello@janessaperry.com>
2025-06-04 11:17:35 -0700

    add AuthProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+// Libraries
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Context
+import { AuthProvider } from "./context/AuthProvider.tsx";
+
+// Components
 import App from './App.tsx'
+
+// Styles
 import './styles/index.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
   </StrictMode>,
 )
