7a399b16136911f808b54ff055338468f5c87e52
janessaperry <hello@janessaperry.com>
2025-06-05 16:56:23 -0700

    remove logs and extra spacing in Auth Layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,9 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth.tsx";
 
 const AuthLayout = () => {
-
   const { isAuthenticated } = useAuth();
-  console.log("isAuthenticated", isAuthenticated);
 
   if ( isAuthenticated ) {
     return <Navigate to="/"/>;
