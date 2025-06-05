1e00cb1af02279b2d9c4b00e0732d99b3de501e2
janessaperry <hello@janessaperry.com>
2025-06-05 16:25:49 -0700

    set up basic validation checks for sign up form; need to refactor and reorganize still

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,15 +1,18 @@
 // Libraries
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Description, Field, Fieldset, Input, Label } from "@headlessui/react";
+import axios from "axios";
 
 // Layouts
 
 // Route Logic
 
 // Components & Assets
+import { Button, Description, Field, Fieldset, Input, Label } from "@headlessui/react";
 import PressStartLogo from "../../assets/logos/press-start-logo--dark.svg"
 
 // Utils
+import useAuth from "../../hooks/useAuth.tsx";
 
 // Pages
 
@@ -17,6 +20,79 @@ import PressStartLogo from "../../assets/logos/press-start-logo--dark.svg"
 import styles from "./SignUpPage.module.css"
 
 const SignUpPage = () => {
+  const { login } = useAuth();
+  /**
+   * validate form fields
+   * if valid:
+   *    - send post request to backend with sign up info
+   *    - set token in local storage
+   *    - call login to set context
+   * if not valid:
+   *    - show error message
+   */
+  const [ formData, setFormData ] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+        ...formData,
+        [ e.target.id ]: e.target.value
+      }
+    )
+  }
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  const validatePassword = (password: string) => {
+    const regex = /^(?=.*[a-z])(?=.*\d).{8,}$/;
+    return regex.test(password);
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let formValid = false;
+
+    const emailValid = validateEmail(formData.email);
+    const passwordValid = validatePassword(formData.password);
+    const confirmPasswordValid = !!formData.confirmPassword && formData.password === formData.confirmPassword;
+    formValid = emailValid && passwordValid && confirmPasswordValid;
+
+
+    if ( !formValid ) {
+      return;
+    }
+
+    await createUser(formData.email, formData.password);
+  }
+
+  const createUser = async (email: string, password: string) => {
+    //todo move this to correct folder after working
+    const response = await axios.post("http://localhost:8080/users", {
+      email,
+      password
+    });
+    const token = response.data.token;
+    localStorage.setItem('token', token);
+    login(token);
+  }
+
+  const testing = async () => {
+    const response = await axios.get("http://localhost:8080");
+    console.log(response.data);
+    return response.data;
+  }
+
+  useEffect(() => {
+    testing();
+  }, [])
+
+
   return (
     <>
       <main className="h-screen">
@@ -29,20 +105,32 @@ const SignUpPage = () => {
             <p>Already have an account? <Link to="/log-in" className={styles.link}>Sign in (update
               link)</Link></p>
 
-            <form>
+            <form onSubmit={handleSubmit}>
               <h1>Sign up</h1>
 
               <Fieldset className="flex flex-col gap-8 border-none">
                 <Field className={`${styles.formField} flex flex-col gap-1`}>
                   <Label htmlFor="email"
                          className={styles.formLabel}>Email <span className={styles.required}>*</span></Label>
-                  <Input id="email" type="email" placeholder="Email" className={styles.formInput}/>
+                  <Input id="email"
+                         name="email"
+                         type="email"
+                         placeholder="Email"
+                         className={styles.formInput}
+                         value={formData.email}
+                         onChange={(e) => handleInputChange(e)}/>
                 </Field>
 
                 <Field className={`${styles.formField} flex flex-col gap-1`}>
                   <Label htmlFor="password"
                          className={styles.formLabel}>Password <span className={styles.required}>*</span></Label>
-                  <Input id="password" type="password" placeholder="Password" className={styles.formInput}/>
+                  <Input id="password"
+                         name="password"
+                         type="password"
+                         placeholder="Password"
+                         className={styles.formInput}
+                         value={formData.password}
+                         onChange={(e) => handleInputChange(e)}/>
                   <Description className="mb-0 text-sm text-secondary">Password should be at least 8 characters
                     including a number and a lowercase letter.</Description>
                 </Field>
@@ -51,22 +139,18 @@ const SignUpPage = () => {
                   <Label htmlFor="confirmPassword"
                          className={styles.formLabel}>Confirm
                     Password <span className={styles.required}>*</span></Label>
-                  <Input id="confirmPassword" type="password" placeholder="Password" className={styles.formInput}/>
+                  <Input id="confirmPassword"
+                         name="confirmPassword"
+                         type="password"
+                         placeholder="Password"
+                         className={styles.formInput}
+                         value={formData.confirmPassword}
+                         onChange={(e) => handleInputChange(e)}/>
                   <Description className="mb-0 text-sm text-secondary">Enter a password update message.</Description>
-                </Field>
-
-                <Field className={`${styles.formField} flex flex-col gap-1`}>
-                  <Label htmlFor="username"
-                         className={styles.formLabel}>Username <span className={styles.required}>*</span></Label>
-                  <Description className="mb-0 text-sm text-secondary">Can't think of a username? Generate one now and
-                    change it later in your settings.</Description>
-                  <Input id="username" type="text" placeholder="Username" className={styles.formInput}/>
-                  <Description className="mb-0 text-sm text-secondary">Username may only contain alphanumeric characters
-                    or single hyphens, and cannot begin or end with a hyphen.</Description>
                 </Field>
               </Fieldset>
 
-              <Button>Create Account</Button>
+              <Button className="mt-6" type="submit">Create Account</Button>
             </form>
           </section>
 
