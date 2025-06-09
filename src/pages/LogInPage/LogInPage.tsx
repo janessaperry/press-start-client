5fd9cd0ef44dff01b17acb6bee37b980b61ddd8d
janessaperry <hello@janessaperry.com>
2025-06-09 16:06:49 -0700

    login page

--- src/pages/LogInPage/LogInPage.tsx
+++ src/pages/LogInPage/LogInPage.tsx
@@ -1,22 +1,61 @@
 // Libraries
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Description, Field, Fieldset, Input, Label } from "@headlessui/react";
+import axios from "axios";
 
 // Layouts
-
 // Route Logic
-
 // Components & Assets
 import PressStartLogo from "../../assets/logos/press-start-logo--dark.svg"
+import { Button, Field, Fieldset, Input, Label } from "@headlessui/react";
 
 // Utils
-
-// Pages
+import useAuth from "../../hooks/useAuth.tsx";
+import { validateEmail, validatePasswordFormat } from "../../utils/validators.ts";
 
 // Styles
 
 
 const LogInPage = () => {
+  const { login } = useAuth();
+  const [ formData, setFormData ] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+        ...formData,
+        [ e.target.id ]: e.target.value
+      }
+    )
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { email, password } = formData;
+
+    const emailValid = validateEmail(email);
+    const passwordFormatValid = validatePasswordFormat(password);
+    const formValid = emailValid && passwordFormatValid;
+
+    if ( !formValid ) {
+      return;
+    }
+
+    const response = await logIn(email, password);
+    const token = response.data.token;
+    localStorage.setItem('token', token);
+    login(token);
+  }
+
+  const logIn = async (email: string, password: string) => {
+    return await axios.post("http://localhost:8080/users/log-in", {
+      email,
+      password
+    });
+  }
+
   return (
     <>
       <main className="h-screen">
@@ -28,30 +67,33 @@ const LogInPage = () => {
           <section className="bg-secondary flex-1 p-12">
             <p>No account? <Link to="/sign-up" className="text-link-tertiary">Create account</Link></p>
 
-            <form>
-              <h1>Sign up</h1>
+            <form onSubmit={handleSubmit}>
+              <h1>Sign in</h1>
 
               <Fieldset className="flex flex-col gap-8 border-none">
                 <Field className={`flex flex-col gap-1`}>
                   <Label htmlFor="email"
                   >Email <span>*</span></Label>
-                  <Input id="email" type="email" placeholder="Email"/>
+                  <Input id="email" name="email" placeholder="Email"
+                         value={formData.email}
+                         onChange={(e) => handleInputChange(e)}/>
                 </Field>
 
                 <Field className={`flex flex-col gap-1`}>
                   <Label htmlFor="password"
                   >Password <span>*</span></Label>
-                  <Input id="password" type="password" placeholder="Password"/>
-                  <Description className="mb-0 text-sm text-secondary">Password should be at least 8 characters
-                    including a number and a lowercase letter.</Description>
+                  <Input id="password"
+                         name="password"
+                         type="password"
+                         placeholder="Password"
+                         value={formData.password}
+                         onChange={(e) => handleInputChange(e)}/>
                 </Field>
-
               </Fieldset>
 
-              <Button>Sign In</Button>
+              <Button className="mt-6" type="submit">Sign In</Button>
             </form>
           </section>
-
         </div>
       </main>
     </>
