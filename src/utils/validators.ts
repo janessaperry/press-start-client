ed7c8426cf70a8ba371e522cee188b3e4f3a77d7
janessaperry <hello@janessaperry.com>
2025-06-09 16:06:02 -0700

    rename validator function for validating password

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -3,8 +3,7 @@ export const validateEmail = (email: string) => {
   return regex.test(email);
 }
 
-//change name of this to validate format or something?
-export const validatePassword = (password: string) => {
+export const validatePasswordFormat = (password: string) => {
   const regex = /^(?=.*[a-z])(?=.*\d).{8,}$/;
   return regex.test(password);
 }
