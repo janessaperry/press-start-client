78798a34633953b81d370b99e1bb33971551e4d6
janessaperry <hello@janessaperry.com>
2025-06-28 17:12:57 -0700

    remove console log from ProtectedRoute

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,7 +3,6 @@ import useAuth from "../hooks/useAuth.tsx";
 
 const ProtectedRoute = () => {
   const { isAuthenticated } = useAuth();
-  console.log(isAuthenticated);
 
   if ( !isAuthenticated ) {
     return <Navigate to="/sign-up"/>;
