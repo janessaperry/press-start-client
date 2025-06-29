74163d2eccf785b713aa831932522b89838d4ff7
janessaperry <hello@janessaperry.com>
2025-06-29 16:19:49 -0700

    change ProtectedRoute to redirect to log-in if not authenticated

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -5,7 +5,7 @@ const ProtectedRoute = () => {
   const { isAuthenticated } = useAuth();
 
   if ( !isAuthenticated ) {
-    return <Navigate to="/sign-up"/>;
+    return <Navigate to="/log-in"/>;
   }
 
   return <Outlet/>
