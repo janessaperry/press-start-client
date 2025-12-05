f862e4e883ff3b3cde1336ece886db11088d849d
janessaperry <hello@janessaperry.com>
2025-12-04 20:25:14 -0800

    refactor(GameCard): simplify props with `ComponentProps<'article'>` and improve game type and platform rendering

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,20 @@
+import { ComponentProps } from "react";
 import { GameOverview } from "../pages/ExplorePage/ExplorePage.tsx";
 import InfoChipList from "./InfoChipList.tsx";
+import InfoChip from "./InfoChip.tsx";
 
 type GameCardProps = {
   gameOverview: GameOverview,
-  colSpanClass?: string
-}
+} & ComponentProps<'article'>
 
-const GameCard = ({ gameOverview, colSpanClass = "" }: GameCardProps) => {
+const GameCard = ({gameOverview, className = "", ...rest}: GameCardProps) => {
   return (
-    <article className={`p-4 bg-primary-700 rounded-2xl flex gap-4 overflow-hidden ${colSpanClass}`}>
-      <div className="basis-1/4 shrink-0">
+    <article className={`p-4 bg-primary-700 rounded-2xl flex gap-4 overflow-hidden ${className}`} {...rest}>
+      <div className="basis-1/4 shrink-0 flex flex-col gap-2">
         <img className="rounded-xl" src={gameOverview.coverUrl} alt={`${gameOverview.name} cover`}/>
+
+        {/*todo add game type here*/}
+        <InfoChip label={gameOverview.gameType}/>
       </div>
 
       <div className="basis-3/4 grow flex flex-col gap-4">
@@ -25,7 +29,7 @@ const GameCard = ({ gameOverview, colSpanClass = "" }: GameCardProps) => {
               Platforms
             </p>
             <div className="flex flex-wrap gap-3">
-              <InfoChipList data={gameOverview.platforms}/>
+              <InfoChipList data={gameOverview.platforms.map(p => ({id: p.id, label: p.abbreviation}))}/>
             </div>
           </div>
         }
