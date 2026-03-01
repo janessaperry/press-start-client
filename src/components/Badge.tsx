8e4e39ebabd8a566ed3ed7e3351a3d8ec828453d
janessaperry <hello@janessaperry.com>
2026-02-28 20:27:49 -0800

    add `xs` size option to `Badge` component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,14 +1,16 @@
 type Props = {
   label: string,
-  size?: "sm" | "md"
+  size?: "xs" | "sm" | "md"
 }
 
 export const BadgeNumber = ({label, size = "sm"}: Props) => {
   const sizeStyleMap = {
+    "xs": "w-10 h-10 text-md",
     "sm": "w-12 h-12 text-xl",
     "md": "w-16 h-16 text-2xl",
   }
-  const badgeColor: string = label === 'n/a' ? 'text-secondary-100 border-secondary-100' : "text-success border-success"
+  const badgeColor: string = label === 'n/a' ? 'text-secondary-100/40 border-secondary-100/40' : "text-success" +
+    " border-success"
 
   return (
     <div className={`shrink-0 font-extrabold flex items-center justify-center border-2 rounded-full ${badgeColor} ${sizeStyleMap[size]}`}>
@@ -17,8 +19,13 @@ export const BadgeNumber = ({label, size = "sm"}: Props) => {
   )
 }
 
-export const BadgeText = ({label}: Props) => {
+export const BadgeText = ({label, size = "sm"}: Props) => {
+  const sizeStyleMap = {
+    "xs": "text-xs",
+    "sm": "text-sm",
+    "md": "text-md"
+  }
   return (
-    <p className="px-1 text-secondary-100 text-center font-semibold uppercase border border-secondary-100 inline-block rounded-sm">{label}</p>
+    <p className={`px-1 text-secondary-100 text-center font-semibold uppercase border border-secondary-100 rounded-sm ${sizeStyleMap[size]}`}>{label}</p>
   )
 }
