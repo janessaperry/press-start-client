81eae9a4065adff38cbaf0f102d362db7314638c
janessaperry <hello@janessaperry.com>
2026-04-09 17:36:29 -0700

    make `variant` and `iconSize` props optional in `ButtonIcon` component

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -1,13 +1,13 @@
 import { ComponentType, ComponentPropsWithoutRef } from "react";
 
 type Props = {
-  variant: 'primary' | 'secondary' | 'ghost',
-  icon: ComponentType<{className?: string}>,
-  iconSize: 'sm' | 'md' | 'lg' | 'xl',
+  variant?: 'primary' | 'secondary' | 'ghost',
+  icon: ComponentType<{ className?: string }>,
+  iconSize?: 'sm' | 'md' | 'lg' | 'xl',
   handleClick: () => void
 } & ComponentPropsWithoutRef<'button'>
 
-const ButtonIcon = ({variant = 'primary', icon: Icon, iconSize = 'md', handleClick, className}: Props) => {
+const ButtonIcon = ({ variant = 'primary', icon: Icon, iconSize = 'md', handleClick, className }: Props) => {
   return (
     <button className={`p-2 button ${variant} ${className ? className : ''}`} onClick={handleClick}>
       <Icon className={`icon-${iconSize}`}/>
