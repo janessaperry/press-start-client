e4cc8c7239c68b097f6560e64445613f3db19e48
janessaperry <hello@janessaperry.com>
2026-07-13 15:31:11 -0700

    refactor Header and Footer: improve layout, add sticky header, and update Footer with account links, logo, and new sections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,83 @@
+import PressStartLogo from "/src/assets/logos/press-start-logo--dark.svg";
 import JpLogo from "../assets/logos/jp-logo.svg"
+import { Link } from "react-router-dom";
+import { Button } from "@headlessui/react";
 import { GithubLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
+import useAuth from "../hooks/useAuth.ts";
 
 const Footer = () => {
+  const { userId, logout } = useAuth();
+  const year = new Date().getFullYear();
+
   return (
-    <footer className="container p-4 text-grey-50 flex flex-col gap-4 items-center md:flex-row md:justify-between md:px-8 md:py-4">
-      <div>
-        Designed & developed by <a href="https://janessaperry.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="link-primary">Janessa Perry</a>
+    <footer className="container px-4 py-8 text-grey-50 md:px-8 space-y-10">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-10">
+        <div className="space-y-3 col-span-1 md:col-span-2">
+          <img src={PressStartLogo} alt="Press Start Logo" className="h-5"/>
+          <div className="space-y-1">
+            <p>Keep track of the games you own across platforms and spend less time
+              deciding what to play next.
+            </p>
+            <span className="text-sm text-secondary-100">&copy; {year} Press Start</span>
+          </div>
+        </div>
+
+        <div className="col-span-1 space-y-2">
+          <p className="font-bold">Explore Games</p>
+          <ul className="space-y-2">
+            <li><Link to="/explore/nintendo" className="hover:text-interactive-primary-hover">Nintendo</Link></li>
+            <li><Link to="/explore/playstation" className="hover:text-interactive-primary-hover">PlayStation</Link></li>
+            <li><Link to="/explore/xbox" className="hover:text-interactive-primary-hover">Xbox</Link></li>
+            <li><Link to="/explore/pc" className="hover:text-interactive-primary-hover">PC</Link></li>
+          </ul>
+        </div>
+
+        <div className="col-span-1 space-y-2">
+          <p className="font-bold">Account</p>
+          {userId ? (
+            <ul className="space-y-2">
+              <li><Link to="/account-settings" className="hover:text-interactive-primary-hover">Settings</Link></li>
+              <li><Button onClick={logout} className="hover:text-interactive-primary-hover">Log out</Button></li>
+            </ul>
+          ) : (
+            <ul className="space-y-2">
+              <li><Link to="/sign-in" className="hover:text-interactive-primary-hover">Sign in</Link></li>
+              <li><Link to="/sign-up" className="hover:text-interactive-primary-hover">Create account</Link></li>
+            </ul>
+          )}
+        </div>
       </div>
 
-      <div className="flex items-center gap-4">
-        <a className="flex"
-          href="https://janessaperry.com"
+      <hr className="border-t border-secondary-200/10"/>
+
+      <div className="flex flex-col md:flex-row items-center md:justify-between space-y-4">
+        <div>
+          Designed & developed by <a href="https://janessaperry.com"
           target="_blank"
-          rel="noopener noreferrer">
-          <img src={JpLogo as string} alt="JP Logo" className="icon-lg"/>
-        </a>
-        <a className="flex"
-          href="https://github.com/janessaperry"
-          target="_blank"
-          rel="noopener noreferrer">
-          <GithubLogoIcon size={24}/>
-        </a>
-        <a className="flex"
-          href="https://www.linkedin.com/in/janessa-perry/"
-          target="_blank"
-          rel="noopener noreferrer">
-          <LinkedinLogoIcon size={24}/>
-        </a>
+          rel="noopener"
+          className="link-primary">Janessa Perry</a>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <a className="flex"
+            href="https://janessaperry.com"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={JpLogo} alt="JP Logo" className="icon-lg"/>
+          </a>
+          <a className="flex"
+            href="https://github.com/janessaperry"
+            target="_blank"
+            rel="noopener noreferrer">
+            <GithubLogoIcon size={24}/>
+          </a>
+          <a className="flex"
+            href="https://www.linkedin.com/in/janessa-perry/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <LinkedinLogoIcon size={24}/>
+          </a>
+        </div>
       </div>
     </footer>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,8 +25,8 @@ const Header = () => {
 
   return (
     <>
-      <header className="container">
-        <div className="max-w-xl mx-auto p-4 md:px-8 md:py-4">
+      <header className="sticky top-0 bg-secondary-900/80 backdrop-blur-lg z-20">
+        <div className="container mx-auto p-4 md:px-8 md:py-4">
           <nav className="flex items-center justify-between">
             <NavLink to={`${userId ? '/explore' : '/'}`}>
               <img src={PressStartLogo} alt="Press Start Logo" className="max-w-[10rem] md:max-w-[16rem]"/>
