e4b3bfcf52eed3391151f3d9ca7b3a73a81be334
janessaperry <hello@janessaperry.com>
2026-08-06 15:43:50 -0700

    refactor Modal: replace custom markup with HeadlessUI Dialog; update styles accordingly

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef } from "react";
-import { Button } from "@headlessui/react";
+import { Button, Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
 import { XIcon } from "@phosphor-icons/react";
 
 import ButtonIcon from "./ButtonIcon.tsx";
@@ -14,8 +14,8 @@ type Props = {
 
 const Modal = ({ children, className, modalOpen, setModalOpen, handleSubmit, handleCancel }: Props) => {
   const onCancel = () => {
-    handleCancel()
-    setModalOpen(false)
+    handleCancel();
+    setModalOpen(false);
   }
 
   const onSubmit = () => {
@@ -24,21 +24,21 @@ const Modal = ({ children, className, modalOpen, setModalOpen, handleSubmit, han
   }
 
   return (
-    <div className={`${className || ''} ${modalOpen ? 'modal-open' : 'hidden'} fixed top-0 w-full h-dvh p-4 bg-blue-900/60 backdrop-blur-lg flex flex-col gap-8`}
-      role="dialog" aria-modal="true" aria-label="Filters">
-      <ButtonIcon handleClick={onCancel} icon={XIcon} variant='ghost' className="self-end"/>
+    <Dialog open={modalOpen} onClose={onCancel} className="relative z-50">
+      <DialogBackdrop className="fixed inset-0 bg-blue-900/60 backdrop-blur-lg"/>
+      <DialogPanel className={`${className || ''} fixed top-0 w-full h-dvh p-4 flex flex-col gap-8`}>
+        <ButtonIcon handleClick={onCancel} icon={XIcon} variant='ghost' className="self-end"/>
 
-      <div className="overflow-y-scroll scrollbar-on-dark">
-        {children}
-      </div>
+        <div className="overflow-y-scroll scrollbar-on-dark">
+          {children}
+        </div>
 
-      <div className="flex items-center gap-4">
-        <Button className="flex-1 button neutral"
-          onClick={onCancel}>Cancel</Button>
-        <Button className="flex-1 button primary"
-          onClick={onSubmit}>Submit</Button>
-      </div>
-    </div>
+        <div className="flex items-center gap-4">
+          <Button className="flex-1 button neutral" onClick={onCancel}>Cancel</Button>
+          <Button className="flex-1 button primary" onClick={onSubmit}>Submit</Button>
+        </div>
+      </DialogPanel>
+    </Dialog>
   )
 }
 
