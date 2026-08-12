0e6588cf9384b4da618732f98b7f681e497ebc9d
janessaperry <hello@janessaperry.com>
2026-08-12 11:06:52 -0700

    update apiClient baseURL to include versioned `/api/v1` path

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api/v1`,
 })
 
 apiClient.interceptors.request.use((config) => {
