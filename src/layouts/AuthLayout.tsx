11c890400a489b30be5cf421124020b2696b7003
janessaperry <hello@janessaperry.com>
2026-08-12 14:21:24 -0700

    update AuthLayout: redesign side graphic and content with new icons, features, and background; adjust theme colors

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import PressStartLogo from "/src/assets/logos/press-start-logo--dark.svg";
-import { InfoIcon } from "@phosphor-icons/react";
+import { GameControllerIcon, ListIcon, MagnifyingGlassIcon } from "@phosphor-icons/react";
 import { Link, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth.ts";
 
@@ -13,15 +13,31 @@ const AuthLayout = () => {
   return (
     <main className="h-screen">
       <div className="h-full flex">
-        <div className="hidden md:flex md:flex-col md:items-start md:justify-between flex-1 p-12 bg-[url(/src/assets/images/sign-up-bg-v3.jpg)] bg-cover bg-no-repeat bg-right">
+        <div className="hidden md:flex md:flex-col md:items-start md:justify-between flex-1 p-12 bg-[url(/src/assets/images/blue-purple-logo-pattern-1024x1280.png)] bg-cover bg-no-repeat bg-right">
           <Link to="/">
             <img src={PressStartLogo}
               alt="Press Start logo"
               className="h-8 drop-shadow-xl drop-shadow-secondary-900"/>
           </Link>
-          <div className="lg:max-w-1/2 flex items-start gap-1.5 bg-primary-900/60 p-2 text-sm rounded-sm"><InfoIcon
-            className="icon-sm shrink-0"/> AI generated image - if you have a gaming related image you'd like you
-            contribute, please reach out!
+          <div className="px-4 py-12 space-y-12 w-full">
+            <h2 className="text-center">Keep your game collection organized.</h2>
+            <div className="grid grid-cols-3 gap-12">
+              <div className="flex flex-col items-center gap-6">
+                <div className="p-4 border-2 border-accent-300/40 rounded-full">
+                  <MagnifyingGlassIcon className="icon-lg lg:icon-xl text-accent-300"/></div>
+                <h3 className="text-accent-300">Explore</h3>
+              </div>
+              <div className="flex flex-col items-center gap-6">
+                <div className="p-4 border-2 border-primary-200/40 rounded-full">
+                  <ListIcon className="icon-lg lg:icon-xl text-primary-200"/></div>
+                <h3 className="text-primary-200">Collect</h3>
+              </div>
+              <div className="flex flex-col items-center gap-6">
+                <div className="p-4 border-2 border-green-500/40 rounded-full">
+                  <GameControllerIcon className="icon-lg lg:icon-xl text-green-500"/></div>
+                <h3 className="text-green-500">Play</h3>
+              </div>
+            </div>
           </div>
         </div>
 
