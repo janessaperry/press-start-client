a761c5e32a384f46bb2837a7177fa65d199c7b79
janessaperry <hello@janessaperry.com>
2026-08-12 17:37:55 -0700

    add LogRocket for production error monitoring and update .env example with LogRocket ID

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-// Libraries
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-// Context
+import LogRocket from 'logrocket'
 import { AuthProvider } from "./context/AuthProvider.tsx";
-
-// Components
 import App from './App.tsx'
-
-// Styles
 import './styles/index.css'
 
+if (import.meta.env.PROD) {
+  LogRocket.init(import.meta.env.VITE_LOGROCKET_ID)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
